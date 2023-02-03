671ee2a6ec6f22262b8e0800b1416bb95a1e0552
AlexeyGStandalone <coder2k@mail.ru>
2023-02-03 22:15:28 +0300

    movement nad rotation

--- car.js
+++ car.js
@@ -4,16 +4,67 @@ class Car {
         this.y = y;
         this.width = width;
         this.height = height;
+
+        this.speed = 0
+        this.acceleration = .2
+        this.maxSpeed = 3
+        this.friction = 0.05
+        this.angle = 0
+
+        // Управление
+        this.controls = new Controls()
+    }
+
+    update() {
+        if (this.controls.forward) {
+            this.speed += this.acceleration
+        }
+
+        if (this.controls.backward) {
+            this.speed -= this.acceleration
+        }
+
+        // Проверка максимальной скорости
+        if (this.speed > this.maxSpeed / 2)
+            this.speed = this.maxSpeed / 2
+
+        if (this.speed < -this.maxSpeed)
+            this.speed = -this.maxSpeed
+
+        // Трение
+        if (this.speed > 0)
+            this.speed -= this.friction
+        if (this.speed < 0)
+            this.speed += this.friction
+        if (Math.abs(this.speed) < this.friction)
+            this.speed = 0
+
+        this.y -= this.speed
+
+        // Рабора с углами
+        if(this.controls.left) {
+            this.angle -= 0.03
+        }
+
+        if(this.controls.right) {
+            this.angle += 0.03
+        }
     }
 
     draw(ctx) {
-        ctx.beginPath();
+        ctx.save()
+        ctx.translate(this.x, this.y)
+        ctx.rotate(-this.angle)
+
+        ctx.beginPath()
         ctx.rect(
-            this.x - this.width / 2,
-            this.y - this.height / 2,
+            -this.width / 2,
+            -this.height / 2,
             this.width,
             this.height
         )
-        ctx.fill();
+        ctx.fill()
+
+        ctx.restore()
     }
 }
--- main.js
+++ main.js
@@ -2,10 +2,20 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
     const canvas=document.getElementById("myCanvas");
     canvas.width=200;
-    canvas.height = window.innerHeight
 
+
+    // Отрисовка машины
     const ctx = canvas.getContext("2d");
     const car=new Car(100,100,30,50);
     car.draw(ctx);
+
+    animate()
+
+    function animate() {
+        car.update()
+        canvas.height = window.innerHeight // очищаем хост
+        car.draw(ctx);
+        requestAnimationFrame(animate)
+    }
 
 });
