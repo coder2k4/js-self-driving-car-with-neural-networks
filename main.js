6acf483ee78cf2977699e96a22b493fdbec5286f
AlexeyGStandalone <coder2k@mail.ru>
2023-02-04 19:52:11 +0300

    road finish

--- main.js
+++ main.js
@@ -1,20 +1,33 @@
-document.addEventListener("DOMContentLoaded", ()=>{
+document.addEventListener("DOMContentLoaded", () => {
 
-    const canvas=document.getElementById("myCanvas");
-    canvas.width=200;
+    const canvas = document.getElementById("myCanvas");
+    canvas.width = 400;
+
+    const road = new Road(canvas.width / 2, canvas.width * 0.9)
+    const lineCenter = road.getLaneCenter(2)
 
 
     // Отрисовка машины
     const ctx = canvas.getContext("2d");
-    const car=new Car(100,100,30,50);
+    const car = new Car(lineCenter, 100, 30, 50);
     car.draw(ctx);
+
+
 
     animate()
 
     function animate() {
         car.update()
         canvas.height = window.innerHeight // очищаем хост
-        car.draw(ctx);
+
+        ctx.save()
+        ctx.translate(0, -car.y + canvas.height*0.5)
+
+        road.draw(ctx)
+        car.draw(ctx)
+
+
+        ctx.restore()
         requestAnimationFrame(animate)
     }
 
